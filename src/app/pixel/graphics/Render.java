0f65feafd4f862ab36b4ac71fc175225da2c31c8
Ogarkov Sergey <coriouse@gmail.com>
2017-01-31 09:09:25 +0300

    screen code

--- src/app/pixel/graphics/Render.java
+++ src/app/pixel/graphics/Render.java
@@ -3,6 +3,7 @@ package app.pixel.graphics;
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -21,11 +22,26 @@ public class Render {
 	private static int GAME_HEIGHT = 250;
 	
 	private static int gameWidth = 0;
-	private static int gameHeight = 0;
-	
+	private static int gameHeight = 0; 
+		
 	
 	private static void getBestSize() {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = toolkit.getScreenSize();
+		boolean done = false;
+		
+		while(!done) {
+			canvasWidth += GAME_WIDTH;
+			canvasHeight += GAME_HEIGHT;
+			if(canvasWidth > screenSize.width || canvasHeight > screenSize.width) {
+				canvasWidth -= GAME_WIDTH;
+				canvasHeight -= GAME_HEIGHT;
+				done = true;
+			}
+		}
 			
+		int xDiff = screenSize.width - canvasWidth;
+		int yDiff = screenSize.height - canvasHeight;
 	}
 	
 	public static void init() {
