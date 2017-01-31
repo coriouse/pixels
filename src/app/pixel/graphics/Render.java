b36e02801358cadd60500f302fe94ab70b8bcfcd
Ogarkov Sergey <coriouse@gmail.com>
2017-01-31 16:16:36 +0300

    full screen

--- src/app/pixel/graphics/Render.java
+++ src/app/pixel/graphics/Render.java
@@ -3,6 +3,8 @@ package app.pixel.graphics;
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -11,57 +13,75 @@ import app.pixel.game.Game;
 
 public class Render {
 
-	
 	private static Frame frame;
 	private static Canvas canvas;
-	
+
 	private static int canvasWidth = 0;
 	private static int canvasHeight = 0;
-			
-	private static int GAME_WIDTH = 400;
-	private static int GAME_HEIGHT = 250;
-	
+
+	private static final int GAME_WIDTH = 800;
+	private static final int GAME_HEIGHT = 250;
+
 	private static int gameWidth = 0;
-	private static int gameHeight = 0; 
-		
-	
+	private static int gameHeight = 0;
+
 	private static void getBestSize() {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		Dimension screenSize = toolkit.getScreenSize();
 		boolean done = false;
-		
-		while(!done) {
+
+		while (!done) {
 			canvasWidth += GAME_WIDTH;
 			canvasHeight += GAME_HEIGHT;
-			if(canvasWidth > screenSize.width || canvasHeight > screenSize.width) {
+			if (canvasWidth > screenSize.width || canvasHeight > screenSize.width) {
 				canvasWidth -= GAME_WIDTH;
 				canvasHeight -= GAME_HEIGHT;
 				done = true;
 			}
 		}
-			
+
 		int xDiff = screenSize.width - canvasWidth;
 		int yDiff = screenSize.height - canvasHeight;
+		int factor = canvasWidth / GAME_WIDTH;
+
+		gameWidth = canvasWidth / factor + xDiff / factor;
+		gameHeight = canvasHeight / factor + yDiff / factor;
+
+		canvasWidth = gameWidth + factor;
+		canvasHeight = gameHeight + factor;
+
 	}
-	
+
+	private static void makeFullScreen() {
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = env.getDefaultScreenDevice();
+		if (gd.isFullScreenSupported()) {
+			frame.setUndecorated(true);
+			gd.setFullScreenWindow(frame);
+		}
+
+	}
+
 	public static void init() {
 		getBestSize();
-		
+
 		frame = new Frame();
 		canvas = new Canvas();
 		canvas.setPreferredSize(new Dimension(canvasWidth, canvasHeight));
 		frame.add(canvas);
+		//makeFullScreen();
 		frame.pack();
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
-		
+
+	
+
 		frame.addWindowListener(new WindowAdapter() {
-			
 			public void windowClosing(WindowEvent e) {
 				Game.quit();
 			}
-		});		
+		});
 		frame.setVisible(true);
-		
+
 	}
 }
